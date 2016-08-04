0657b84ccc2696fa89daf9f973946d4a18fc0495
Zqw <578159259@qq.com>
2016-08-04 15:35:08 +0800

    主界面侧滑显示菜单

--- JiaoBao2/BaseView/BaseViewController.m
+++ JiaoBao2/BaseView/BaseViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "BaseViewController.h"
+#import "VKSideMenu.h"
 
-@interface BaseViewController ()
+@interface BaseViewController ()<VKSideMenuDelegate, VKSideMenuDataSource>
+
+@property (nonatomic, strong) VKSideMenu *menu;
 
 @end
 
@@ -31,6 +34,10 @@
     self.navigationItem.rightBarButtonItem=[[UIBarButtonItem alloc]initWithCustomView:rightbutton];
     
     self.navigationItem.title = @"教宝";
+    
+    self.menu = [[VKSideMenu alloc] initWithWidth:220 andDirection:VKSideMenuDirectionLeftToRight];
+    self.menu.dataSource = self;
+    self.menu.delegate   = self;
 }
 
 - (void) viewDidDisappear:(BOOL)animated{
@@ -41,10 +48,93 @@
 
 -(void)SaoMiao{
     NSLog(@"SaoMiao");
+    [self.menu show];
 }
 -(void)Search{
     NSLog(@"Search");
 }
+#pragma mark - VKSideMenuDataSource
+-(NSInteger)numberOfSectionsInSideMenu:(VKSideMenu *)sideMenu
+{
+    return 1;
+}
+
+-(NSInteger)sideMenu:(VKSideMenu *)sideMenu numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+-(VKSideMenuItem *)sideMenu:(VKSideMenu *)sideMenu itemForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    VKSideMenuItem *item = [VKSideMenuItem new];
+    
+    switch (indexPath.row)
+    {
+        case 0:
+            item.title = @"Profile";
+            item.icon  = [UIImage imageNamed:@"ic_option_1"];
+            break;
+            
+        case 1:
+            item.title = @"Messages";
+            item.icon  = [UIImage imageNamed:@"ic_option_2"];
+            break;
+            
+        case 2:
+            item.title = @"Cart";
+            item.icon  = [UIImage imageNamed:@"ic_option_3"];
+            break;
+            
+        case 3:
+            item.title = @"Settings";
+            item.icon  = [UIImage imageNamed:@"ic_option_4"];
+            break;
+            
+        default:
+            break;
+    }
+    
+    return item;
+}
+
+#pragma mark - VKSideMenuDelegate
+
+-(void)sideMenu:(VKSideMenu *)sideMenu didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"SideMenu didSelectRow: %@", indexPath);
+}
+
+-(void)sideMenuDidShow:(VKSideMenu *)sideMenu
+{
+    NSLog(@"VKSideMenue did show");
+}
+
+-(void)sideMenuDidHide:(VKSideMenu *)sideMenu
+{
+    NSLog(@"VKSideMenue did hide");
+}
+
+-(NSString *)sideMenu:(VKSideMenu *)sideMenu titleForHeaderInSection:(NSInteger)section
+{
+//    if (sideMenu == self.menuLeft)
+        return nil;
+//
+//    switch (section)
+//    {
+//        case 0:
+//            return @"Profile";
+//            break;
+//            
+//        case 1:
+//            return @"Actions";
+//            break;
+//            
+//        default:
+//            return nil;
+//            break;
+//    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
